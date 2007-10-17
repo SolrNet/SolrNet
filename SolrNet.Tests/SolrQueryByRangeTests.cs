b8ecc2bb31fd01b85477326a0d92434a98165da5
Mauricio Scheffer <mauricioscheffer@gmail.com>
2007-10-17 23:10:04 +0000

    query by range
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@27 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ SolrNet.Tests/SolrQueryByRangeTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace SolrNet.Tests {
+	[TestFixture]
+	public class SolrQueryByRangeTests {
+		public class TestDocument : ISolrDocument {}
+
+		[Test]
+		public void IntInclusive() {
+			ISolrQuery<TestDocument> q = new SolrQueryByRange<TestDocument, int>("id", 123, 456);
+			Assert.AreEqual("id:[123 TO 456]", q.Query);
+		}
+
+		[Test]
+		public void StringInclusive() {
+			ISolrQuery<TestDocument> q = new SolrQueryByRange<TestDocument, string>("id", "Arroz", "Zimbabwe");
+			Assert.AreEqual("id:[Arroz TO Zimbabwe]", q.Query);
+		}
+
+		[Test]
+		public void StringExclusive() {
+			ISolrQuery<TestDocument> q = new SolrQueryByRange<TestDocument, string>("id", "Arroz", "Zimbabwe", false);
+			Assert.AreEqual("id:{Arroz TO Zimbabwe}", q.Query);
+		}
+	}
+}
---
+++ SolrNet/SolrQueryByRange.cs
@@ -0,0 +1,22 @@
+namespace SolrNet {
+	public class SolrQueryByRange<T, RT> : ISolrQuery<T> where T : ISolrDocument {
+		private string q;
+		public SolrQueryByRange(string fieldName, RT from, RT to) : this(fieldName, from, to, true) {}
+
+		public SolrQueryByRange(string fieldName, RT from, RT to, bool inclusive) {
+			q = "$field:$ii$from TO $to$if"
+				.Replace("$field", fieldName)
+				.Replace("$ii", inclusive ? "[" : "{")
+				.Replace("$if", inclusive ? "]" : "}")
+				.Replace("$from", from.ToString())
+				.Replace("$to", to.ToString());
+		}
+
+		/// <summary>
+		/// query string
+		/// </summary>
+		public string Query {
+			get { return q; }
+		}
+	}
+}

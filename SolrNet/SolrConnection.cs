46bffb4e16240ece6fe900a63ca3fb89f2772d17
Mauricio Scheffer <mauricioscheffer@gmail.com>
2008-02-25 16:18:00 +0000

    http keepalive=false. C# 3 refactorings.
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@76 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

--- SolrNet/SolrConnection.cs
+++ SolrNet/SolrConnection.cs
@@ -10,123 +10,123 @@ using SolrNet.Exceptions;
 using SolrNet.Utils;
 
 namespace SolrNet {
-	public class SolrConnection : ISolrConnection {
-		private string serverURL;
-		private readonly IHttpWebRequestFactory httpWebRequestFactory = new HttpWebRequestFactory();
-		private Encoding xmlEncoding = Encoding.UTF8;
-		private string version = "2.2";
+    public class SolrConnection : ISolrConnection {
+        private readonly IHttpWebRequestFactory httpWebRequestFactory = new HttpWebRequestFactory();
+        private string serverURL;
+        private string version = "2.2";
+        private Encoding xmlEncoding = Encoding.UTF8;
 
-		public SolrConnection(string serverURL) {
-			ServerURL = serverURL;
-		}
+        public SolrConnection(string serverURL) {
+            ServerURL = serverURL;
+        }
 
-		public SolrConnection(string serverURL, IHttpWebRequestFactory httpWebRequestFactory) {
-			ServerURL = serverURL;
-			this.httpWebRequestFactory = httpWebRequestFactory;
-		}
+        public SolrConnection(string serverURL, IHttpWebRequestFactory httpWebRequestFactory) {
+            ServerURL = serverURL;
+            this.httpWebRequestFactory = httpWebRequestFactory;
+        }
 
-		public string ServerURL {
-			get { return serverURL; }
-			set {
-				try {
-					Uri u = new Uri(value);
-					if (u.Scheme != Uri.UriSchemeHttp && u.Scheme != Uri.UriSchemeHttps)
-						throw new InvalidURLException("Only HTTP or HTTPS protocols are supported");
-				} catch (ArgumentException e) {
-					throw new InvalidURLException(e);
-				} catch (UriFormatException e) {
-					throw new InvalidURLException(e);
-				}
-				serverURL = value;
-			}
-		}
+        #region ISolrConnection Members
 
-		/// <summary>
-		/// Solr XML response syntax version
-		/// </summary>
-		public string Version {
-			get { return version; }
-			set { version = value; }
-		}
+        public string ServerURL {
+            get { return serverURL; }
+            set {
+                try {
+                    var u = new Uri(value);
+                    if (u.Scheme != Uri.UriSchemeHttp && u.Scheme != Uri.UriSchemeHttps)
+                        throw new InvalidURLException("Only HTTP or HTTPS protocols are supported");
+                } catch (ArgumentException e) {
+                    throw new InvalidURLException(e);
+                } catch (UriFormatException e) {
+                    throw new InvalidURLException(e);
+                }
+                serverURL = value;
+            }
+        }
 
-		public Encoding XmlEncoding {
-			get { return xmlEncoding; }
-			set { xmlEncoding = value; }
-		}
+        /// <summary>
+        /// Solr XML response syntax version
+        /// </summary>
+        public string Version {
+            get { return version; }
+            set { version = value; }
+        }
 
-		public static byte[] ReadFully(Stream stream) {
-			byte[] buffer = new byte[32768];
-			using (MemoryStream ms = new MemoryStream()) {
-				while (true) {
-					int read = stream.Read(buffer, 0, buffer.Length);
-					if (read <= 0)
-						return ms.ToArray();
-					ms.Write(buffer, 0, read);
-				}
-			}
-		}
+        public Encoding XmlEncoding {
+            get { return xmlEncoding; }
+            set { xmlEncoding = value; }
+        }
 
-		public string Post(string relativeUrl, string s) {
-			Console.WriteLine(s);
-			UriBuilder u = new UriBuilder(serverURL);
-			u.Path += relativeUrl;
-			IHttpWebRequest request = httpWebRequestFactory.Create(u.Uri);
-			request.Method = HttpWebRequestMethod.POST;
-			request.ContentType = "text/xml; charset=utf-8";
-			request.ContentLength = s.Length;
-			request.ProtocolVersion = HttpVersion.Version10;
-			try {
-				using (Stream postParams = request.GetRequestStream()) {
-					postParams.Write(xmlEncoding.GetBytes(s), 0, s.Length);
-					using (IHttpWebResponse response = request.GetResponse()) {
-						using (Stream rStream = response.GetResponseStream()) {
-							string r = xmlEncoding.GetString(ReadFully(rStream));
-							//Console.WriteLine(r);
-							return r;
-						}
-					}
-				}
-			} catch (WebException e) {
-				throw new SolrConnectionException(e);
-			}
-		}
+        public string Post(string relativeUrl, string s) {
+            Console.WriteLine(s);
+            var u = new UriBuilder(serverURL);
+            u.Path += relativeUrl;
+            var request = httpWebRequestFactory.Create(u.Uri);
+            request.Method = HttpWebRequestMethod.POST;
+            request.KeepAlive = false;
+            request.ContentType = "text/xml; charset=utf-8";
+            request.ContentLength = s.Length;
+            request.ProtocolVersion = HttpVersion.Version10;
+            try {
+                using (var postParams = request.GetRequestStream()) {
+                    postParams.Write(xmlEncoding.GetBytes(s), 0, s.Length);
+                    using (var response = request.GetResponse()) {
+                        using (var rStream = response.GetResponseStream()) {
+                            string r = xmlEncoding.GetString(ReadFully(rStream));
+                            //Console.WriteLine(r);
+                            return r;
+                        }
+                    }
+                }
+            } catch (WebException e) {
+                throw new SolrConnectionException(e);
+            }
+        }
 
-		public string Get(string relativeUrl, IDictionary<string, string> parameters) {
-			UriBuilder u = new UriBuilder(serverURL);
-			u.Path += relativeUrl;
-			if (parameters == null)
-				parameters = new Dictionary<string, string>();
-			parameters["version"] = version;
-			// TODO clean up, too messy
-			u.Query = Func.Reduce(
-				Func.Map<KeyValuePair<string, string>, string>(parameters,
-				                                               delegate(KeyValuePair<string, string> input) {
-				                                               	return
-				                                               		string.Format("{0}={1}", HttpUtility.UrlEncode(input.Key),
-				                                               		              HttpUtility.UrlEncode(input.Value));
-				                                               }), "?",
-				delegate(string x, string y) {
-					return string.Format("{0}&{1}", x, y);
-				});
-			//Console.WriteLine(u.Uri);
-			IHttpWebRequest request = httpWebRequestFactory.Create(u.Uri);
-			request.Method = HttpWebRequestMethod.GET;
-			request.ProtocolVersion = HttpVersion.Version10; // for some reason Version11 throws
-			try {
-				using (IHttpWebResponse response = request.GetResponse()) {
-					using (Stream rStream = response.GetResponseStream()) {
-						return xmlEncoding.GetString(ReadFully(rStream));
-					}
-				}
-			} catch (WebException e) {
-				if (e.Response != null) {
-					IHttpWebResponse r = new HttpWebResponseAdapter(e.Response);
-					if (r.StatusCode == HttpStatusCode.BadRequest) {
-						throw new InvalidFieldException(r.StatusDescription, e);
-					}
-				}
-				throw new SolrConnectionException(e);
-			}
-		}
-	}
+        public string Get(string relativeUrl, IDictionary<string, string> parameters) {
+            var u = new UriBuilder(serverURL);
+            u.Path += relativeUrl;
+            if (parameters == null)
+                parameters = new Dictionary<string, string>();
+            parameters["version"] = version;
+            // TODO clean up, too messy
+            u.Query = Func.Reduce(
+                Func.Map(parameters, input => string.Format("{0}={1}", HttpUtility.UrlEncode(input.Key),
+                                                            HttpUtility.UrlEncode(input.Value))), "?",
+                ( x, y ) => string.Format("{0}&{1}", x, y));
+            //Console.WriteLine(u.Uri);
+            var request = httpWebRequestFactory.Create(u.Uri);
+            request.Method = HttpWebRequestMethod.GET;
+            request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version10; // for some reason Version11 throws
+            try {
+                using (var response = request.GetResponse()) {
+                    using (var rStream = response.GetResponseStream()) {
+                        return xmlEncoding.GetString(ReadFully(rStream));
+                    }
+                }
+            } catch (WebException e) {
+                if (e.Response != null) {
+                    var r = new HttpWebResponseAdapter(e.Response);
+                    if (r.StatusCode == HttpStatusCode.BadRequest) {
+                        throw new InvalidFieldException(r.StatusDescription, e);
+                    }
+                }
+                throw new SolrConnectionException(e);
+            }
+        }
+
+        #endregion
+
+        public static byte[] ReadFully(Stream stream) {
+            var buffer = new byte[32768];
+            using (var ms = new MemoryStream()) {
+                while (true) {
+                    int read = stream.Read(buffer, 0, buffer.Length);
+                    if (read <= 0)
+                        return ms.ToArray();
+                    ms.Write(buffer, 0, read);
+                }
+            }
+        }
+    }
 }

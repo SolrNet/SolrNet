6b8efce02459d7bf222f0a5e1b3d5796098f4e36
Mauricio Scheffer <mauricioscheffer@gmail.com>
2008-09-20 19:26:10 +0000

    sample tests
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@113 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ SolrNet.Tests/Integration.Sample/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolrNet.Tests.Integration.Sample {
+	public class Product : ISolrDocument {
+		[SolrField("id")]
+		[SolrUniqueKey]
+		public string Id { get; set; }
+
+		[SolrField("sku")]
+		public string SKU { get; set; }
+
+		[SolrField("name")]
+		public string Name { get; set; }
+
+		[SolrField("manu")]
+		public string Manufacturer { get; set; }
+
+		[SolrField("cat")]
+		public ICollection<string> Categories { get; set; }
+
+		[SolrField("features")]
+		public ICollection<string> Features { get; set; }
+
+		[SolrField("includes")]
+		public string Includes { get; set; }
+
+		[SolrField("weight")]
+		public float Weight { get; set; }
+
+		[SolrField("price")]
+		public decimal Price { get; set; }
+
+		[SolrField("popularity")]
+		public int Popularity { get; set; }
+
+		[SolrField("inStock")]
+		public bool InStock { get; set; }
+
+		[SolrField("word")]
+		public string Word { get; set; }
+
+		[SolrField("timestamp")]
+		public DateTime Timestamp { get; set; }
+	}
+}
---
+++ SolrNet.Tests/Integration.Sample/Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace SolrNet.Tests.Integration.Sample {
+	/// <summary>
+	/// These tests run with the sample schema that comes with solr
+	/// </summary>
+	[TestFixture]
+	public class Tests {
+		private const string serverURL = "http://localhost:8983/solr";
+
+		[Test]
+		public void Add() {
+			var p = new Product {
+				Id = "SP2514N",
+				Name = "Samsung SpinPoint P120 SP2514N - hard drive - 250 GB - ATA-133",
+				Manufacturer = "Samsung Electronics Co. Ltd.",
+        Categories = new[] {
+        	"electronics",
+          "hard drive",
+				},
+				Features = new[] {
+					"7200RPM, 8MB cache, IDE Ultra ATA-133",
+          "NoiseGuard, SilentSeek technology, Fluid Dynamic Bearing (FDB) motor",
+				},
+				Price = 92,
+				Popularity = 6,
+				InStock = true,
+			};
+
+			var solr = new SolrServer<Product>(serverURL);
+			solr.Delete(new SolrQuery("*:*"));
+			solr.Add(p);
+			solr.Commit();
+			var products = solr.Query(new SolrQueryByRange<decimal>("price", 10m, 100m));
+			Assert.AreEqual(1, products.Count);
+			Assert.AreEqual("SP2514N", products[0].Id);
+		}
+	}
+}

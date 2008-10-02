ff1c96d8a6a7b53258a576d382f436193108206b
Mauricio Scheffer <mauricioscheffer@gmail.com>
2008-10-02 04:06:37 +0000

    documentation
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@120 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

--- SolrNet/Commands/OptimizeCommand.cs
+++ SolrNet/Commands/OptimizeCommand.cs
@@ -3,8 +3,17 @@ using System.Xml;
 
 namespace SolrNet.Commands {
 	public class OptimizeCommand : ISolrCommand {
+
+		/// <summary>
+		/// Block until index changes are flushed to disk
+		/// Default is true
+		/// </summary>
 		public bool? WaitFlush { get; set; }
 
+		/// <summary>
+		/// Block until a new searcher is opened and registered as the main query searcher, making the changes visible. 
+		/// Default is true
+		/// </summary>
 		public bool? WaitSearcher { get; set; }
 
 		public string Execute(ISolrConnection connection) {
--- SolrNet/Commands/Parameters/QueryOptions.cs
+++ SolrNet/Commands/Parameters/QueryOptions.cs
@@ -2,11 +2,38 @@
 
 namespace SolrNet.Commands.Parameters {
 	public class QueryOptions {
+		/// <summary>
+		/// Fields to retrieve.
+		/// By default, all stored fields are returned
+		/// </summary>
 		public ICollection<string> Fields { get; set; }
+
+		/// <summary>
+		/// Collection of facet queries
+		/// </summary>
 		public ICollection<ISolrFacetQuery> FacetQueries { get; set; }
+
+		/// <summary>
+		/// Offset in the complete result set for the queries where the set of returned documents should begin
+		/// Default is 0
+		/// </summary>
 		public int? Start { get; set; }
+
+		/// <summary>
+		/// Maximum number of documents from the complete result set to return to the client for every request.
+		/// Default is 10
+		/// </summary>
 		public int? Rows { get; set; }
+
+		/// <summary>
+		/// Sort order.
+		/// By default, it's "score desc"
+		/// </summary>
 		public ICollection<SortOrder> OrderBy { get; set; }
+
+		/// <summary>
+		/// Highlighting parameters
+		/// </summary>
 		public HighlightingParameters Highlight { get; set; }
 
 		public QueryOptions() {

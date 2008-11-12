7eca692f560763eccd664a340d65c2f58b586348
Mauricio Scheffer <mauricioscheffer@gmail.com>
2008-11-12 01:50:55 +0000

    AllPropertiesMappingManager
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@129 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ SolrNet.Tests/AllPropertiesMappingManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace SolrNet.Tests {
+	[TestFixture]
+	public class AllPropertiesMappingManagerTests {
+		[Test]
+		public void GetFields() {
+			var m = new AllPropertiesMappingManager();
+			var fields = m.GetFields(typeof(Entity));
+			Assert.AreEqual(2, fields.Count);
+			foreach (var f in fields) {
+				if (f.Value == "Id")
+					Assert.AreEqual("Id", f.Key.Name);
+				else if (f.Value == "Description")
+					Assert.AreEqual("Description", f.Key.Name);
+				else
+					Assert.Fail("Invalid field '{0}'", f.Value);
+			}
+		}
+
+		[Test]
+		public void Get_and_set_unique_key() {
+			var m = new AllPropertiesMappingManager();
+			m.SetUniqueKey(typeof(Entity).GetProperty("Id"));
+			var pk = m.GetUniqueKey(typeof (Entity));
+			Assert.IsNotNull(pk);
+			Assert.IsNotNull(pk.Key);
+			Assert.IsNotNull(pk.Value);
+			Assert.AreEqual("Id", pk.Key.Name);
+			Assert.AreEqual("Id", pk.Value);
+		}
+
+		public class Entity {
+			public int Id { get; set; }
+
+			public string Description { get; set; }
+		}
+	}
+}
---
+++ SolrNet/AllPropertiesMappingManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using SolrNet.Utils;
+
+namespace SolrNet {
+	/// <summary>
+	/// Maps all properties in the class, with the same field name as the property
+	/// </summary>
+	public class AllPropertiesMappingManager : IReadOnlyMappingManager {
+		private readonly IDictionary<Type, PropertyInfo> uniqueKeys = new Dictionary<Type, PropertyInfo>();
+
+		public ICollection<KeyValuePair<PropertyInfo, string>> GetFields(Type type) {
+			var props = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+			var kvProps = Func.Select(props, prop => new KeyValuePair<PropertyInfo, string>(prop, prop.Name));
+			return new List<KeyValuePair<PropertyInfo, string>>(kvProps);
+		}
+
+		public KeyValuePair<PropertyInfo, string> GetUniqueKey(Type type) {
+			var key = uniqueKeys[type];
+			return new KeyValuePair<PropertyInfo, string>(key, key.Name);
+		}
+
+		public void SetUniqueKey(PropertyInfo property) {
+			if (property == null)
+				throw new ArgumentNullException("property");
+			var t = property.ReflectedType;
+			uniqueKeys[t] = property;			
+		}
+	}
+}

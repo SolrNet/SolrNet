ee140c21fdfe594d0cf5f68bb531267ad58c3b23
Mauricio Scheffer <mauricioscheffer@gmail.com>
2008-12-05 13:33:26 +0000

    wrap multiqueries in parentheses
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@132 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

--- SolrNet.Tests/SolrMultipleCriteriaQueryTests.cs
+++ SolrNet.Tests/SolrMultipleCriteriaQueryTests.cs
@@ -2,34 +2,34 @@ using System;
 using NUnit.Framework;
 
 namespace SolrNet.Tests {
-	[TestFixture]
-	public class SolrMultipleCriteriaQueryTests {
-		public class TestDocument : ISolrDocument {}
+    [TestFixture]
+    public class SolrMultipleCriteriaQueryTests {
+        public class TestDocument : ISolrDocument {}
 
-		[Test]
-		public void Concat() {
-			var q1 = new SolrQuery("1");
-			var q2 = new SolrQuery("2");
-			var qm = new SolrMultipleCriteriaQuery(new[] {q1, q2});
-			Assert.AreEqual("1  2", qm.Query);
-		}
+        [Test]
+        public void Concat() {
+            var q1 = new SolrQuery("1");
+            var q2 = new SolrQuery("2");
+            var qm = new SolrMultipleCriteriaQuery(new[] {q1, q2});
+            Assert.AreEqual("(1  2)", qm.Query);
+        }
 
         [Test]
         public void Concat_different_types() {
             var q1 = new SolrQuery("1");
             var q2 = new SolrQueryByField("f", "v");
-            var qm = new SolrMultipleCriteriaQuery(new ISolrQuery[] { q1, q2 });
+            var qm = new SolrMultipleCriteriaQuery(new ISolrQuery[] {q1, q2});
             Console.WriteLine(qm.Query);
-            Assert.AreEqual("1  f:v", qm.Query);            
+            Assert.AreEqual("(1  f:v)", qm.Query);
         }
 
 
-		[Test]
-		public void AcceptsNulls() {
-			var q1 = new SolrQuery("1");
-			ISolrQuery q2 = null;
-			var qm = new SolrMultipleCriteriaQuery(new[] {q1, q2});
-			Assert.AreEqual("1", qm.Query);
-		}
-	}
+        [Test]
+        public void AcceptsNulls() {
+            var q1 = new SolrQuery("1");
+            ISolrQuery q2 = null;
+            var qm = new SolrMultipleCriteriaQuery(new[] {q1, q2});
+            Assert.AreEqual("(1)", qm.Query);
+        }
+    }
 }

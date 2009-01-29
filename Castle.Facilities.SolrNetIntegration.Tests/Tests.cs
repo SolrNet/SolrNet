c146549e4fa5cac0c15f404840c5df2b5f5d592b
Mauricio Scheffer <mauricioscheffer@gmail.com>
2009-01-29 01:16:24 +0000

    issue #31 windsor facility
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@168 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ Castle.Facilities.SolrNetIntegration.Tests/Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Castle.Core.Configuration;
+using Castle.MicroKernel.Facilities;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using CommonServiceLocator.WindsorAdapter;
+using Microsoft.Practices.ServiceLocation;
+using NUnit.Framework;
+using SolrNet;
+
+namespace Castle.Facilities.SolrNetIntegration.Tests {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        [ExpectedException(typeof(FacilityException))]
+        public void NoConfig_throws() {
+            var container = new WindsorContainer();
+            container.AddFacility<SolrNetFacility>();
+        }
+
+        [Test]
+        [ExpectedException(typeof(FacilityException))]
+        public void InvalidUrl_throws() {
+            var configStore = new DefaultConfigurationStore();
+            var configuration = new MutableConfiguration("solr");
+            configuration.CreateChild("solrURL", "123");
+            configStore.AddFacilityConfiguration("solr", configuration);
+            var container = new WindsorContainer(configStore);
+            container.AddFacility<SolrNetFacility>("solr");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FacilityException))]
+        public void InvalidProtocol_throws() {
+            var configStore = new DefaultConfigurationStore();
+            var configuration = new MutableConfiguration("solr");
+            configuration.CreateChild("solrURL", "ftp://localhost");
+            configStore.AddFacilityConfiguration("solr", configuration);
+            var container = new WindsorContainer(configStore);
+            container.AddFacility<SolrNetFacility>("solr");
+        }
+
+        [Test]
+        public void Ping_Query() {
+            var configStore = new DefaultConfigurationStore();
+            var configuration = new MutableConfiguration("solr");
+            configuration.CreateChild("solrURL", "http://localhost:8983/solr");
+            configStore.AddFacilityConfiguration("solr", configuration);
+            var container = new WindsorContainer(configStore);
+            container.AddFacility<SolrNetFacility>("solr");
+
+            var locator = new WindsorServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => locator);
+
+            var solr = container.Resolve<ISolrOperations<Document>>();
+            solr.Ping();
+            Console.WriteLine(solr.Query(SolrQuery.All).Count);
+        }
+
+        public class Document {}
+    }
+}
---
+++ Castle.Facilities.SolrNetIntegration/SolrNetFacility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Castle.MicroKernel.Facilities;
+using Castle.MicroKernel.Registration;
+using SolrNet;
+using SolrNet.Utils;
+
+namespace Castle.Facilities.SolrNetIntegration {
+    public class SolrNetFacility : AbstractFacility {
+        private readonly string solrURL;
+
+        public SolrNetFacility() {}
+
+        public SolrNetFacility(string solrURL) {
+            ValidateUrl(solrURL);
+            this.solrURL = solrURL;
+        }
+
+        private string GetSolrUrl() {
+            if (solrURL != null)
+                return solrURL;
+            if (FacilityConfig == null)
+                throw new FacilityException("Please add solrURL to the SolrNetFacility configuration");
+            var configNode = FacilityConfig.Children["solrURL"];
+            if (configNode == null)
+                throw new FacilityException("Please add solrURL to the SolrNetFacility configuration");
+            var url = configNode.Value;
+            ValidateUrl(url);
+            return url;
+        }
+
+        protected override void Init() {
+            var mapper = new MemoizingMappingManager(new AttributesMappingManager());
+            Kernel.AddComponentInstance<IReadOnlyMappingManager>(mapper);
+            Kernel.Register(Component.For<IRNG>().ImplementedBy<RNG>());
+            Kernel.Register(Component.For<IListRandomizer>().ImplementedBy<ListRandomizer>());
+            Kernel.Register(Component.For<ISolrConnection>().ImplementedBy<SolrConnection>()
+                                .Parameters(Parameter.ForKey("serverURL").Eq(GetSolrUrl())));
+            Kernel.Register(Component.For(typeof (ISolrQueryResultParser<>)).ImplementedBy(typeof (SolrQueryResultParser<>)));
+            Kernel.Register(Component.For(typeof (ISolrQueryExecuter<>)).ImplementedBy(typeof (SolrQueryExecuter<>)));
+            Kernel.Register(Component.For(typeof(ISolrDocumentSerializer<>)).ImplementedBy(typeof(SolrDocumentSerializer<>)));
+            Kernel.Register(Component.For(typeof(ISolrBasicOperations<>), typeof(ISolrBasicReadOnlyOperations<>))
+                .ImplementedBy(typeof(SolrBasicServer<>)));
+            Kernel.Register(Component.For(typeof(ISolrOperations<>), typeof(ISolrReadOnlyOperations<>))
+                .ImplementedBy(typeof(SolrServer<>)));
+        }
+
+        private static void ValidateUrl(string url) {
+            try {
+                var u = new Uri(url);
+                if (u.Scheme != Uri.UriSchemeHttp && u.Scheme != Uri.UriSchemeHttps)
+                    throw new FacilityException("Only HTTP or HTTPS protocols are supported");
+            } catch (ArgumentException e) {
+                throw new FacilityException(string.Format("Invalid URL '{0}'", url), e);
+            } catch (UriFormatException e) {
+                throw new FacilityException(string.Format("Invalid URL '{0}'", url), e);
+            }
+        }
+    }
+}

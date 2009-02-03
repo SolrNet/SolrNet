23b687b9ab0c70ad85fb80ee8ad46d39deefc687
Mauricio Scheffer <mauricioscheffer@gmail.com>
2009-02-03 02:07:25 +0000

    experimenting with route parameters instead of directly changing the query string. Didn't work as expected...
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@183 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ SampleSolrApp/Helpers/UrlHelperRouteExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleSolrApp.Helpers {
+    public static class UrlHelperRouteExtensions {
+        public static string SetRouteParameters(this UrlHelper helper, IDictionary<string, object> parameters) {
+            var routeParams = new RouteValueDictionary(helper.RequestContext.RouteData.Values);
+            foreach (var p in parameters)
+                routeParams[p.Key] = p.Value;
+            return helper.RouteUrl(routeParams);
+        }
+
+        public static string SetRouteParameters(this UrlHelper helper, object parameters) {
+            return helper.SetRouteParameters(parameters.ToPropertyDictionary());
+        }
+
+        public static string SetRouteParameter(this UrlHelper helper, string key, object value) {
+            return helper.SetRouteParameters(new Dictionary<string, object> {
+                {key, value},
+            });
+        }
+    }
+}

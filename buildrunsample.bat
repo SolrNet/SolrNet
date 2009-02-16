929c683b755cd5429b75521137478f5d169abb96
Mauricio Scheffer <mauricioscheffer@gmail.com>
2009-02-16 02:09:59 +0000

    runsample and buildrunsample
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@244 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ buildrunsample.bat
@@ -0,0 +1,3 @@
+@echo off
+msbuild
+runsample
--- runsample.bat
+++ runsample.bat
@@ -1,5 +1,4 @@
 @echo off
-msbuild
 cd solr-1.3.0
 start java -jar start.jar
 cd ..
@@ -8,3 +7,4 @@ ping 127.0.0.1 -n 2 -w 2000 > nul
 tools\curl -I -m5 http://localhost:8983/solr >nul
 tools\curl -I -m5 http://localhost:8082 >nul
 start http://localhost:8082/
+exit

63243bf09f3420ccd2d1547820df501a8bb54dc0
Mauricio Scheffer <mauricioscheffer@gmail.com>
2009-03-29 21:55:15 +0000

    some TypeHelper tests
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@329 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

---
+++ SolrNet.Tests/TypeHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SolrNet.Utils;
+
+namespace SolrNet.Tests {
+    [TestFixture]
+    public class TypeHelperTests {
+        [Test]
+        public void IsGenericAssignableFrom() {
+            Assert.IsTrue(TypeHelper.IsGenericAssignableFrom(typeof (IDictionary<,>), typeof (Dictionary<string, string>)));
+        }
+
+        [Test]
+        public void IsNotGenericAssignableFrom() {
+            Assert.IsFalse(TypeHelper.IsGenericAssignableFrom(typeof(IDictionary<,>), typeof(List<string>)));
+        }
+
+        [Test]
+        public void IsNotGenericAssignableFrom_non_generic() {
+            Assert.IsFalse(TypeHelper.IsGenericAssignableFrom(typeof(IList), typeof(List<string>)));
+        }
+
+        [Test]
+        public void IsNullableType() {
+            Assert.IsTrue(TypeHelper.IsNullableType(typeof (int?)));
+        }
+
+        [Test]
+        public void IsNotNullableType() {
+            Assert.IsFalse(TypeHelper.IsNullableType(typeof(int)));
+        }
+
+    }
+}

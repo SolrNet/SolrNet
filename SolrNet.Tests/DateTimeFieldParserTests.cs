11bfdd24575b8198f73851f8cee46a254c830b3a
Mauricio Scheffer <mauricioscheffer@gmail.com>
2009-12-30 16:44:14 +0000

    round-trip date serialization tests
    git-svn-id: http://solrnet.googlecode.com/svn/trunk@487 66c3f25c-543c-0410-ae2e-6f2ca0bd8c61

--- SolrNet.Tests/DateTimeFieldParserTests.cs
+++ SolrNet.Tests/DateTimeFieldParserTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using MbUnit.Framework;
 using SolrNet.Impl.FieldParsers;
+using SolrNet.Impl.FieldSerializers;
 
 namespace SolrNet.Tests {
     [TestFixture]
@@ -15,8 +16,28 @@ namespace SolrNet.Tests {
         }
 
         public IEnumerable<object[]> DataFactory() {
-            yield return new object[] {"1-01-01T00:00:00Z", new DateTime(1, 1, 1)};
+            yield return new object[] { "1-01-01T00:00:00Z", new DateTime(1, 1, 1) };
             yield return new object[] { "2004-11-01T00:00:00Z", new DateTime(2004, 11, 1) };
         }
+
+        [Test]
+        [Factory("DateTimes")]
+        public void RoundTrip(DateTime dt) {
+            var parser = new DateTimeFieldParser();
+            var serializer = new DateTimeFieldSerializer();
+            var s = serializer.SerializeDate(dt);
+            Console.WriteLine(s);
+            var value = parser.ParseDate(s);
+            Console.WriteLine(value.ToString("r"));
+            Assert.AreEqual(dt, value);
+        }
+
+        public IEnumerable<object> DateTimes() {
+            yield return new DateTime(1, 1, 1);
+            yield return new DateTime(2004, 11, 1);
+            yield return new DateTime(2004, 11, 1, 15, 41, 23);
+            yield return new DateTime(2008, 5, 6, 14, 21, 23, 0, DateTimeKind.Local);
+        }
+
     }
 }

82128d9f5301bd4f6f5e73715e42bff9087e51b4
mausch <mauricioscheffer@gmail.com>
2010-05-08 23:40:28 -0300

    xml doc, fixed namespace

--- SolrNet/Mapping/Validation/Rules/ISolrFieldTypeChecker.cs
+++ SolrNet/Mapping/Validation/Rules/ISolrFieldTypeChecker.cs
@@ -17,10 +17,20 @@
 #endregion
 
 using System;
-using SolrNet.Mapping.Validation;
+using SolrNet.Schema;
 
-namespace SolrNet.Schema {
-    public interface ISolrFieldTypeChecker{
+namespace SolrNet.Mapping.Validation.Rules {
+    /// <summary>
+    /// Validates a mapped document property against a Solr field type
+    /// </summary>
+    public interface ISolrFieldTypeChecker {
+        /// <summary>
+        /// Validates a mapped document property against a Solr field type
+        /// </summary>
+        /// <param name="solrFieldType"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyType"></param>
+        /// <returns></returns>
         MappingValidationItem Validate(SolrFieldType solrFieldType, string propertyName, Type propertyType);
     }
 }

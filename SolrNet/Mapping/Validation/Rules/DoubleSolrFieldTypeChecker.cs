80ec2582c625650a373968c4efbeee325e808a2d
mausch <mauricioscheffer@gmail.com>
2010-05-08 23:53:22 -0300

    refactored field type checkers to abstract class

--- SolrNet/Mapping/Validation/Rules/DoubleSolrFieldTypeChecker.cs
+++ SolrNet/Mapping/Validation/Rules/DoubleSolrFieldTypeChecker.cs
@@ -16,35 +16,10 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using SolrNet.Schema;
-
 namespace SolrNet.Mapping.Validation.Rules {
-    public class DoubleSolrFieldTypeChecker : ISolrFieldTypeChecker
-    {
+    public class DoubleSolrFieldTypeChecker : AbstractSolrFieldTypeChecker {
         public DoubleSolrFieldTypeChecker()
-        {
-            SafeTypes = new List<string> { "solr.TrieDoubleField", "solr.SortableDoubleField", "solr.DoubleField" };
-            WarningTypes = new List<string> { "solr.TextField", "solr.StrField" };
-        }
-
-        public List<string> SafeTypes { get; set; }
-        public List<string> WarningTypes { get; set; }
-
-        #region ISolrFieldTypeChecker Members
-
-        public MappingValidationItem Validate(SolrFieldType solrFieldType, string propertyName, Type propertyType)
-        {
-            // Check if the type is in the safe or warning types and otherwise return a error
-            if (SafeTypes != null && SafeTypes.Contains(solrFieldType.Type))
-                return null;
-            if (WarningTypes != null && WarningTypes.Contains(solrFieldType.Type))
-                return new MappingValidationWarning(String.Format("Property '{0}' of type '{1}' is mapped to a solr field of type '{2}'. These types are not fully compatible.", propertyName, propertyType.FullName, solrFieldType.Name));
-
-            return new MappingValidationError(String.Format("Property '{0}' of type '{1}' cannot be stored in solr field type '{2}'.", propertyName, propertyType.FullName, solrFieldType.Name));
-        }
-
-        #endregion
+            : base(new[] {"solr.TrieDoubleField", "solr.SortableDoubleField", "solr.DoubleField"},
+                   new[] {"solr.TextField", "solr.StrField"}) {}
     }
 }

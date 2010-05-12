de95547c275ce32a52e71c6229a4abd7d323d0ee
mausch <mauricioscheffer@gmail.com>
2010-05-12 18:20:57 -0300

    xml docs

--- SolrNet/Impl/ISolrCache.cs
+++ SolrNet/Impl/ISolrCache.cs
@@ -16,10 +16,20 @@
 
 namespace SolrNet.Impl {
     /// <summary>
-    /// HTTP cache
+    /// HTTP-level cache for Solr responses
     /// </summary>
     public interface ISolrCache {
+        /// <summary>
+        /// Gets a cached Solr response. Returns null if there is no cached response for this URL
+        /// </summary>
+        /// <param name="url">Full Solr query URL (including all querystring parameters)</param>
+        /// <returns></returns>
         SolrCacheEntity this[string url] { get; }
+
+        /// <summary>
+        /// Adds a Solr response to the cache
+        /// </summary>
+        /// <param name="e"></param>
         void Add(SolrCacheEntity e);
     }
 }

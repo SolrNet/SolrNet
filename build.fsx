fe98fe8f9f1b587d42d2f2fa55bc5a20ba551f3d
mausch <mauricioscheffer@gmail.com>
2010-11-09 13:38:51 -0300

    moved helper functions to a separate script

--- build.fsx
+++ build.fsx
@@ -2,41 +2,16 @@
 #r "FakeLib.dll"
 #r "Fake.Gallio.dll"
 #r "System.Xml.Linq"
+#load "fake.fsx"
 
 open System
 open System.Xml.Linq
 open Fake
 
 let version = "0.3.0"
-let solr = "solr-1.4.0"
 let buildDir = "merged"
 let config = getBuildParamOrDefault "config" "debug"
 let target = getBuildParamOrDefault "target" "BuildAll"
-
-// helper functions
-let (.>) (a: #seq<XElement>) (b: string) = Extensions.Descendants(a, xname b)
-let startsWith s (n: XElement) = n.Value.StartsWith s
-let contains s (n: XElement) = n.Value.Contains s
-let setValue s (n: XElement) = n.Value <- s
-let replaceValue orig repl (n: XElement) = n.Value <- n.Value.Replace((orig:string), (repl:string))
-let httpGet = Fake.REST.ExecuteGetCommand null null
-
-module Solr =
-    let private cmdline = "-DSTOP.PORT=8079 -DSTOP.KEY=secret -jar start.jar"
-    let private execParam cmd = 
-        { WorkingDirectory = solr
-          Program = "java"
-          CommandLine = cmdline + " " + cmd
-          Args = [] }
-    let start() = 
-        asyncShellExec (execParam "") |> Async.StartAsTask |> ignore
-        let watch = System.Diagnostics.Stopwatch.StartNew()
-        while httpGet "http://localhost:8983/solr/admin/ping" = null do
-            if watch.Elapsed > (TimeSpan.FromSeconds 10.)
-                then failwith "Solr test instance didn't work"
-            System.Threading.Thread.Sleep 500
-    let stop() =
-        shellExec (execParam "--stop") |> ignore
 
 let slnBuild sln x = 
     sln |> build (fun p -> { p with 
---
+++ fake.fsx
@@ -0,0 +1,34 @@
+﻿#I @"lib"
+#r "FakeLib.dll"
+#r "System.Xml.Linq"
+
+open System
+open System.Xml.Linq
+open Fake
+
+let solr = "solr-1.4.0"
+
+// helper functions
+let (.>) (a: #seq<XElement>) (b: string) = Extensions.Descendants(a, xname b)
+let startsWith s (n: XElement) = n.Value.StartsWith s
+let contains s (n: XElement) = n.Value.Contains s
+let setValue s (n: XElement) = n.Value <- s
+let replaceValue orig repl (n: XElement) = n.Value <- n.Value.Replace((orig:string), (repl:string))
+let httpGet = Fake.REST.ExecuteGetCommand null null
+
+module Solr =
+    let private cmdline = "-DSTOP.PORT=8079 -DSTOP.KEY=secret -jar start.jar"
+    let private execParam cmd = 
+        { WorkingDirectory = solr
+          Program = "java"
+          CommandLine = cmdline + " " + cmd
+          Args = [] }
+    let start() = 
+        asyncShellExec (execParam "") |> Async.StartAsTask |> ignore
+        let watch = System.Diagnostics.Stopwatch.StartNew()
+        while httpGet "http://localhost:8983/solr/admin/ping" = null do
+            if watch.Elapsed > (TimeSpan.FromSeconds 10.)
+                then failwith "Solr test instance didn't work"
+            System.Threading.Thread.Sleep 500
+    let stop() =
+        shellExec (execParam "--stop") |> ignore

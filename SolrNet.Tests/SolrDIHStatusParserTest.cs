702615025fbd520fc7e019c6b154d3a1cabaa40a
alukantsov <alukantsov@autobase.net>
2010-12-23 13:36:32 -0500

    Fixed typo DHI to DIH
    Fixed SolrNet.Tests project so it compiles.

--- SolrNet.Tests/SolrDIHStatusParserTest.cs
+++ SolrNet.Tests/SolrDIHStatusParserTest.cs
@@ -15,21 +15,21 @@ namespace SolrNet.Tests
         {
             var DIHStatusParser = new SolrDIHStatusParser();
             XmlDocument xml = EmbeddedResource.GetEmbeddedXml(GetType(), "Resources.solrDIHStatusBasic.xml");
-            SolrDIHStatus dhiStatusDoc = DIHStatusParser.Parse(xml);
+            SolrDIHStatus dihStatusDoc = DIHStatusParser.Parse(xml);
 
-            Assert.AreEqual(1, dhiStatusDoc.TotalRequestToDataSource);
-            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dhiStatusDoc.Committed);
-            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dhiStatusDoc.FullDumpStarted);
-            Assert.AreEqual("", dhiStatusDoc.ImportResponse);
-            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dhiStatusDoc.Optimized);
-            Assert.AreEqual(DIHStatus.IDLE, dhiStatusDoc.Status);
-            Assert.AreEqual("\r\n      Indexing completed. Added/Updated: 764648 documents. Deleted 0 documents.\r\n    ", dhiStatusDoc.Summary);
-            Assert.AreEqual(new TimeSpan(), dhiStatusDoc.TimeElapsed);
-            Assert.AreEqual(new TimeSpan(0, 0, 9, 48, 65), dhiStatusDoc.TimeTaken);
-            Assert.AreEqual(735352, dhiStatusDoc.TotalDocumentsFailed);
-            Assert.AreEqual(764648, dhiStatusDoc.TotalDocumentsProcessed);
-            Assert.AreEqual(0, dhiStatusDoc.TotalDocumentsSkipped);
-            Assert.AreEqual(1500000, dhiStatusDoc.TotalRowsFetched);
+            Assert.AreEqual(1, dihStatusDoc.TotalRequestToDataSource);
+            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dihStatusDoc.Committed);
+            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dihStatusDoc.FullDumpStarted);
+            Assert.AreEqual("", dihStatusDoc.ImportResponse);
+            Assert.AreEqual(DateTime.Parse("2010-12-09 00:00:00"), dihStatusDoc.Optimized);
+            Assert.AreEqual(DIHStatus.IDLE, dihStatusDoc.Status);
+            Assert.AreEqual("\r\n      Indexing completed. Added/Updated: 764648 documents. Deleted 0 documents.\r\n    ", dihStatusDoc.Summary);
+            Assert.AreEqual(new TimeSpan(), dihStatusDoc.TimeElapsed);
+            Assert.AreEqual(new TimeSpan(0, 0, 9, 48, 65), dihStatusDoc.TimeTaken);
+            Assert.AreEqual(735352, dihStatusDoc.TotalDocumentsFailed);
+            Assert.AreEqual(764648, dihStatusDoc.TotalDocumentsProcessed);
+            Assert.AreEqual(0, dihStatusDoc.TotalDocumentsSkipped);
+            Assert.AreEqual(1500000, dihStatusDoc.TotalRowsFetched);
         }
 
     }

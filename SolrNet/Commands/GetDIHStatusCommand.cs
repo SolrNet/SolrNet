3a68320849f63ebff2a15dcc39fdfe48cbfe4340
mausch <mauricioscheffer@gmail.com>
2011-02-19 18:07:29 -0300

    code reformat

--- SolrNet/Commands/GetDIHStatusCommand.cs
+++ SolrNet/Commands/GetDIHStatusCommand.cs
@@ -1,23 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using SolrNet.Commands.Parameters;
 
-namespace SolrNet.Commands
-{
+namespace SolrNet.Commands {
     /// <summary>
     /// Gets the raw Solr schema
     /// </summary>
-    public class GetDIHStatusCommand : ISolrCommand
-    {
+    public class GetDIHStatusCommand : ISolrCommand {
         private KeyValuePair<string, string> options;
 
         /// <summary>
         /// Get DIH Status Command
         /// </summary>
         /// <param name="options"></param>
-        public GetDIHStatusCommand(KeyValuePair<string, string> options)
-        {
+        public GetDIHStatusCommand(KeyValuePair<string, string> options) {
             this.options = options;
         }
 
@@ -26,8 +21,7 @@ namespace SolrNet.Commands
         /// </summary>
         /// <param name="connection"></param>
         /// <returns></returns>
-        public string Execute(ISolrConnection connection)
-        {
+        public string Execute(ISolrConnection connection) {
             return connection.Get("/dataimport", new[] { new KeyValuePair<string, string>() });
         }
     }

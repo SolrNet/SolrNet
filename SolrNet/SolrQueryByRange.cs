28440171da76846d0114887caff5baef34a1ce52
mausch <mauricioscheffer@gmail.com>
2011-06-18 22:10:30 -0300

    changelog; xmldocs; inclusive not obsolete yet until Solr 4 is released

--- SolrNet/SolrQueryByRange.cs
+++ SolrNet/SolrQueryByRange.cs
@@ -31,10 +31,32 @@ namespace SolrNet {
         private readonly bool inclusiveFrom;
         private readonly bool inclusiveTo;
 
+        /// <summary>
+        /// Creates an range query with inclusive bounds
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
         public SolrQueryByRange(string fieldName, RT from, RT to) : this(fieldName, from, to, true) {}
 
+        /// <summary>
+        /// Creates a range query
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="inclusive">Inclusive bounds</param>
         public SolrQueryByRange(string fieldName, RT @from, RT to, bool inclusive) : this(fieldName, from, to, inclusive, inclusive) {}
 
+        /// <summary>
+        /// Creates a range query.
+        /// Different bounds inclusiveness ONLY available in Solr 4.0+
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="inclusiveFrom">Lower bound inclusive</param>
+        /// <param name="inclusiveTo">Upper bound inclusive</param>
         public SolrQueryByRange(string fieldName, RT @from, RT to, bool inclusiveFrom, bool inclusiveTo) {
             this.fieldName = fieldName;
             this.from = from;
@@ -63,7 +85,9 @@ namespace SolrNet {
             get { return to; }
         }
 
-        [Obsolete("Use InclusiveFrom and InclusiveTo", false)]
+        /// <summary>
+        /// Is lower and upper bound inclusive
+        /// </summary>
         public bool Inclusive {
             get { return inclusiveFrom && inclusiveTo; }
         }

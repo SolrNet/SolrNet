cf4fad5c211c29c7aa4864a7cb7d37679032df0e
mausch <mauricioscheffer@gmail.com>
2011-08-13 13:01:05 -0300

    documentation

--- SolrNet/Commands/Parameters/RegexFlag.cs
+++ SolrNet/Commands/Parameters/RegexFlag.cs
@@ -5,12 +5,44 @@ namespace SolrNet.Commands.Parameters {
     public abstract class RegexFlag {
         internal RegexFlag() {}
 
+        /// <summary>
+        /// By default, case-insensitive matching assumes that only characters in the US-ASCII charset are being matched. 
+        /// Unicode-aware case-insensitive matching can be enabled by specifying the <see cref="UnicodeCase"/> flag in conjunction with this flag.
+        /// </summary>
         public static readonly RegexFlag CaseInsensitive = new RegexFlagCaseInsensitive();
+
+        /// <summary>
+        /// In this mode, whitespace is ignored, and embedded comments starting with # are ignored until the end of a line.
+        /// </summary>
         public static readonly RegexFlag Comments = new RegexFlagComments();
+
+        /// <summary>
+        /// In multiline mode the expressions ^ and $ match just after or just before, respectively, a line terminator or the end of the input sequence. 
+        /// By default these expressions only match at the beginning and the end of the entire input sequence.
+        /// </summary>
         public static readonly RegexFlag MultiLine = new RegexFlagMultiLine();
+
+        /// <summary>
+        /// In dotall mode, the expression . matches any character, including a line terminator. By default this expression does not match line terminators.
+        /// </summary>
         public static readonly RegexFlag DotAll = new RegexFlagDotAll();
+
+        /// <summary>
+        /// When this flag is specified then case-insensitive matching, when enabled by the CASE_INSENSITIVE flag, is done in a manner consistent with the Unicode Standard. 
+        /// By default, case-insensitive matching assumes that only characters in the US-ASCII charset are being matched.
+        /// </summary>
         public static readonly RegexFlag UnicodeCase = new RegexFlagUnicodeCase();
+
+        /// <summary>
+        /// When this flag is specified then two characters will be considered to match if, and only if, their full canonical decompositions match. 
+        /// The expression "a\u030A", for example, will match the string "å" when this flag is specified. 
+        /// By default, matching does not take canonical equivalence into account.
+        /// </summary>
         public static readonly RegexFlag CanonEq = new RegexFlagCanonEq();
+
+        /// <summary>
+        /// In this mode, only the '\n' line terminator is recognized in the behavior of ., ^, and $.
+        /// </summary>
         public static readonly RegexFlag UnixLines = new RegexFlagUnixLines();
 
         private class RegexFlagCaseInsensitive: RegexFlag {

2c643492efecd86af892e7ad80fcde0cba8c9efb
mausch <mauricioscheffer@gmail.com>
2011-09-11 14:10:08 -0300

    bumped version

--- build.fsx
+++ build.fsx
@@ -10,7 +10,7 @@ open System.Xml.Linq
 open Fake
 open Fake.FileUtils
 
-let version = "0.4.0.1001"
+let version = "0.4.0.2001"
 let buildDir = "merged"
 let nugetDir = "nuget"
 let nugetDocs = nugetDir @@ "content"

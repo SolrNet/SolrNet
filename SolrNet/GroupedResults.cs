ef21a1a306fc98f8c53738c38ecc0beec2687835
mausch <mauricioscheffer@gmail.com>
2011-11-06 21:58:34 -0300

    split Group to its own file

--- SolrNet/GroupedResults.cs
+++ SolrNet/GroupedResults.cs
@@ -1,22 +1,21 @@
 ﻿using System.Collections.Generic;
 using SolrNet.Commands.Parameters;
 
-namespace SolrNet
-{
-	/// <summary>
-	/// GroupedResults<typeparamref name="T"/> contains all the results for one group
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public class GroupedResults<T>
-	{
-		/// <summary>
-		/// Returns the number of matching (unique!!) documents that are grouped. 
-		/// </summary>
-		public int Matches { get; set; }
-		/// <summary>
-		/// Grouped documents 
-		/// </summary>
-		public ICollection<Group<T>> Groups { get; set; }
+namespace SolrNet {
+    /// <summary>
+    /// GroupedResults<typeparamref name="T"/> contains all the results for one group
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GroupedResults<T> {
+        /// <summary>
+        /// Returns the number of matching (unique!!) documents that are grouped. 
+        /// </summary>
+        public int Matches { get; set; }
+
+        /// <summary>
+        /// Grouped documents 
+        /// </summary>
+        public ICollection<Group<T>> Groups { get; set; }
 
         /// <summary>
         /// Number of groups that have matched the query.
@@ -24,39 +23,11 @@ namespace SolrNet
         /// </summary>
         public int? Ngroups { get; set; }
 
-		/// <summary>
-		/// Constructur for Groups
-		/// </summary>
-		public GroupedResults()
-		{
-			Groups = new List<Group<T>>();
-		}
-	}
-
-	/// <summary>
-	/// A Single group of documents
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public class Group<T>
-	{
-		/// <summary>
-		/// The groupvalue for this group of documents
-		/// </summary>
-		public string GroupValue { get; set; }
-
-		/// <summary>
-		/// Returns the number of matching documents that are found for this groupValue
-		/// </summary>
-		public int NumFound { get; set; }
-
-		/// <summary>
-		/// The actual documents in the group.
-		/// You can control the amount of documents in this collection by using the Limit property of the GroupingParameters
-		/// </summary>
-		public ICollection<T> Documents { get; set; }
-
-		public Group() {
-			Documents = new List<T>();
-		}
-	}
+        /// <summary>
+        /// Constructur for Groups
+        /// </summary>
+        public GroupedResults() {
+            Groups = new List<Group<T>>();
+        }
+    }
 }

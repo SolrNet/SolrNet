b31c8be4894ddaa1b1a8b9c77e3eec5203b1c3bb
Stephen Lacy <stephen@lacy.ie>
2012-04-03 18:45:45 +0100

    Reverted changes to QueryByFieldSerializer made in 38e47695b940d5ae46bf0ee8ae1d4f14008c4ac4

--- SolrNet/Impl/QuerySerializers/QueryByFieldSerializer.cs
+++ SolrNet/Impl/QuerySerializers/QueryByFieldSerializer.cs
@@ -22,16 +22,16 @@ namespace SolrNet.Impl.QuerySerializers {
         public override string Serialize(SolrQueryByField q) {
             if (q.FieldName == null || q.FieldValue == null)
                 return null;
-            string r = SpecialCharactersRx.Replace(q.FieldValue, "\\$1");
-            return string.Format("{0}:({1})", q.FieldName, q.Quoted ? Quote(r) : r);
+            return string.Format("({0}:{1})", q.FieldName, q.Quoted ? Quote(q.FieldValue) : q.FieldValue);
         }
 
         public static readonly Regex SpecialCharactersRx = new Regex("(\\+|\\-|\\&\\&|\\|\\||\\!|\\{|\\}|\\[|\\]|\\^|\\(|\\)|\\\"|\\~|\\:|\\;|\\\\|\\?|\\*)", RegexOptions.Compiled);
 
         public static string Quote(string value) {
-            if (value.IndexOf(' ') != -1 || value == "")
-                value = string.Format("\"{0}\"", value);
-            return value;
+            string r = SpecialCharactersRx.Replace(value, "\\$1");
+            if (r.IndexOf(' ') != -1 || r == "")
+                r = string.Format("\"{0}\"", r);
+            return r;
         }
     }
 }

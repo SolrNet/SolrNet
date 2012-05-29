8f95f4266b25090e07564bd9cf19f4771a5602f8
Stephane Erbrech <stephane.erbrech@gmail.com>
2012-05-29 14:34:21 +0200

    DataImportHandler
    Basic implementation
    - Handles custom handler name
    - Handles CommandType (full-import, delta-import)

---
+++ SolrNet.Tests/DIHCommandTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using SolrNet.Commands;
+using SolrNet.Tests.Mocks;
+using SolrNet.Utils;
+
+namespace SolrNet.Tests {
+    [TestFixture]
+    public class DIHCommandTests {
+        private readonly IEnumerable<KeyValuePair<string, string>> emptyParameters = new KeyValuePair<string, string>[0]; 
+
+        [Test]
+        public void BasicDataImportTest() {
+            var conn = AssertGet(new DIHCommand(), "/dataimport", emptyParameters);
+            Assert.AreEqual(1, conn.get.Calls);
+        }
+
+        [Test]
+        public void NamedDataImportTest() {
+            var cmd = new DIHCommand {HandlerName = "custom-name"};
+            var conn = AssertGet(cmd, "/custom-name", emptyParameters);
+            Assert.AreEqual(1, conn.get.Calls);
+        }
+
+        [Test]
+        public void CommandTypeFullImportTest() {
+            var cmd = new DIHCommand {CommandType = DIHCommand.DIHCommandType.FullImport};
+            var conn = AssertGet(cmd, "/dataimport", new[] {
+                KV.Create("command", "full-import")
+            });
+            Assert.AreEqual(1, conn.get.Calls);
+        }
+
+        [Test]
+        public void CommandTypeDelatImportTest() {
+            var cmd = new DIHCommand {CommandType = DIHCommand.DIHCommandType.DeltaImport};
+            var expectedParameters = new[] {
+                KV.Create("command", "delta-import")
+            };
+            var conn = AssertGet(cmd, "/dataimport", expectedParameters);
+            Assert.AreEqual(1, conn.get.Calls);
+        }
+
+        private static MSolrConnection AssertGet(DIHCommand cmd, string expectedUrl, IEnumerable<KeyValuePair<string, string>> parameters) {
+            return AssertGet(cmd, (s, pairs) => {
+                Assert.AreEqual(expectedUrl, s);
+                Assert.AreElementsEqualIgnoringOrder(parameters, pairs);
+                Console.WriteLine(parameters);
+                return null;
+            });
+        }
+
+        private static MSolrConnection AssertGet(DIHCommand cmd, Func<string, IEnumerable<KeyValuePair<string, string>>, string> get = null, Func<string, IEnumerable<KeyValuePair<string, string>>, string> post = null) {
+            var conn = new MSolrConnection();
+            conn.get += get;
+            cmd.Execute(conn);
+            return conn;
+        }
+    }
+}
---
+++ SolrNet/Commands/DIHCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolrNet.Utils;
+
+namespace SolrNet.Commands
+{
+    /// <summary>
+    /// Request solr to perform a data import.
+    /// </summary>
+    public class DIHCommand : ISolrCommand
+    {
+        public enum DIHCommandType
+        {
+            None,
+            FullImport,
+            DeltaImport
+        }
+
+        /// <summary>
+        /// Name of the data import request handler.
+        /// Default is dataimport.
+        /// </summary>
+        public string HandlerName { get; set; }
+
+        /// <summary>
+        /// Define what kind of import to perform. 
+        /// If no CommandType is set, the handler returns the status (e.g.: idle).
+        /// </summary>
+        public DIHCommandType CommandType { get; set; }
+
+        public DIHCommand()
+        {
+            HandlerName = "dataimport";
+        }
+
+
+        public string Execute(ISolrConnection connection)
+        {
+            string url = string.Format("/{0}", HandlerName);
+            var parameters = GetParameters();
+            return connection.Get(url, parameters);
+        }
+
+        private IEnumerable<KeyValuePair<string, string>> GetParameters()
+        {
+            if (CommandType != DIHCommandType.None)
+                yield return KV.Create("command", CommandValue(CommandType));
+        }
+
+        private string CommandValue(DIHCommandType commandType)
+        {
+            switch (commandType)
+            {
+                case DIHCommandType.FullImport:
+                    return "full-import";
+                case DIHCommandType.DeltaImport:
+                    return "delta-import";
+                default:
+                    return "";
+            }
+        }
+    }
+}

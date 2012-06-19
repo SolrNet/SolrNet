4fbbf324662cc2a6c2b039591253463515dac1db
mausch <mauricioscheffer@gmail.com>
2012-06-19 20:42:51 -0300

    Code style

--- SolrNet/Impl/TermVectorResults.cs
+++ SolrNet/Impl/TermVectorResults.cs
@@ -1,63 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-namespace SolrNet.Impl
-{
-	/// <summary>
-	/// TermVector results
-	/// </summary>
-	public class TermVectorResults : ICollection<TermVectorDocumentResult>
-	{
-		/// <summary>
-		/// TermVector results
-		/// </summary>
+namespace SolrNet.Impl {
+    /// <summary>
+    /// TermVector results
+    /// </summary>
+    public class TermVectorResults : ICollection<TermVectorDocumentResult> {
+        /// <summary>
+        /// TermVector results
+        /// </summary>
+        private readonly IList<TermVectorDocumentResult> TermVectors = new List<TermVectorDocumentResult>();
 
-		private readonly IList<TermVectorDocumentResult> TermVectors = new List<TermVectorDocumentResult>();
 
-		
-		public IEnumerator<TermVectorDocumentResult> GetEnumerator()
-		{
-			return TermVectors.GetEnumerator();
-		}
+        public IEnumerator<TermVectorDocumentResult> GetEnumerator() {
+            return TermVectors.GetEnumerator();
+        }
 
-		public void Add(TermVectorDocumentResult item)
-		{
-			TermVectors.Add(item);
-		}
+        public void Add(TermVectorDocumentResult item) {
+            TermVectors.Add(item);
+        }
 
-		public void Clear()
-		{
-			TermVectors.Clear();
-		}
+        public void Clear() {
+            TermVectors.Clear();
+        }
 
-		public bool Contains(TermVectorDocumentResult item)
-		{
-			return TermVectors.Contains(item);
-		}
+        public bool Contains(TermVectorDocumentResult item) {
+            return TermVectors.Contains(item);
+        }
 
-		public void CopyTo(TermVectorDocumentResult[] array, int arrayIndex)
-		{
-			TermVectors.CopyTo(array, arrayIndex);
-		}
+        public void CopyTo(TermVectorDocumentResult[] array, int arrayIndex) {
+            TermVectors.CopyTo(array, arrayIndex);
+        }
 
-		public bool Remove(TermVectorDocumentResult item)
-		{
-			return TermVectors.Remove(item);
-		}
+        public bool Remove(TermVectorDocumentResult item) {
+            return TermVectors.Remove(item);
+        }
 
-		public int Count
-		{
-			get { return TermVectors.Count; }
-		}
+        public int Count {
+            get { return TermVectors.Count; }
+        }
 
-		public bool IsReadOnly
-		{
-			get { return TermVectors.IsReadOnly; }
-		}
+        public bool IsReadOnly {
+            get { return TermVectors.IsReadOnly; }
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
-	}
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
 }

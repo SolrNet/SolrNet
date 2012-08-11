198d409b0c6a28e87aa524a6147269d6233387ad
mausch <mauricioscheffer@gmail.com>
2012-08-11 16:24:24 -0300

    Move BasicAuthHttpWebRequestFactory to a separate file

--- HttpWebAdapters/Impl/HttpWebRequestFactory.cs
+++ HttpWebAdapters/Impl/HttpWebRequestFactory.cs
@@ -28,32 +28,4 @@ namespace HttpWebAdapters {
 			return new HttpWebRequestAdapter((HttpWebRequest) WebRequest.Create(url));
 		}
 	}
-
-    public class BasicAuthHttpWebRequestFactory : IHttpWebRequestFactory
-    {
-        private string username;
-        private string password;
-
-        public BasicAuthHttpWebRequestFactory(string username, string password)
-        {
-            this.username = username;
-            this.password = password;
-        }
-
-        public IHttpWebRequest Create(string url)
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(username + ":" + password));
-            req.Headers.Add("Authorization", "Basic " + credentials);
-            return new HttpWebRequestAdapter(req);
-        }
-
-        public IHttpWebRequest Create(Uri url)
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(username + ":" + password));
-            req.Headers.Add("Authorization", "Basic " + credentials);
-            return new HttpWebRequestAdapter(req);
-        }
-    }
 }

f35a97ae2163f6ba7df8ab4af3262449a0dae704
Stephen Pope <stp@sitecore.net>
2012-09-19 14:09:14 +0100

    Forgot to save Ninject file :)

--- Ninject.Integration.SolrNet/SolrNetModule.cs
+++ Ninject.Integration.SolrNet/SolrNetModule.cs
@@ -190,6 +190,7 @@ namespace Ninject.Integration.SolrNet {
 
             if (cores.Count != 0)
             {
+                Bind<ISolrStatusResponseParser>().To<SolrStatusResponseParser>();
                 Bind<ISolrCoreAdmin>().To<SolrCoreAdmin>();
 
                 foreach (var solrCore in cores)

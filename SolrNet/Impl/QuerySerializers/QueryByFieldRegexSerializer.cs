58bf990e40b0f98df18e1f98e0343043441655cb
Stephen Pope <stp@sitecore.net>
2013-01-08 17:22:28 +0000

    Initial commit + tests

---
+++ SolrNet/Impl/QuerySerializers/QueryByFieldRegexSerializer.cs
@@ -0,0 +1,24 @@
+﻿// 
+namespace SolrNet.Impl.QuerySerializers 
+{
+    /// <summary>
+    /// Serializes a SolrQueryByFieldRegex query.
+    /// </summary>
+    public class QueryByFieldRegexSerializer : SingleTypeQuerySerializer<SolrQueryByFieldRegex>
+    {
+        /// <summary>
+        /// Serializes a SolrQueryByFieldRegex query.
+        /// </summary>
+        /// <param name="q">The query.</param>
+        /// <returns></returns>
+        public override string Serialize(SolrQueryByFieldRegex q)
+        {
+            if (q == null || q.FieldName == null || q.Expression == null)
+            {
+                return null;
+            }
+
+            return string.Format("{0}:/{1}/", QueryByFieldSerializer.EscapeSpaces(q.FieldName), q.Expression);
+        }
+    }
+}

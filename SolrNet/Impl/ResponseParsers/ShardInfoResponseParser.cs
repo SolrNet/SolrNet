401fbf930c7e8f06c61db49ce63c75eb12e68c14
Tiernan OToole <tiernan@tiernanotoole.ie>
2013-02-18 12:00:21 +0000

    Revert "more tweaks to ShardInfo fix"
    This reverts commit bdf8f0d90b1f68f5b423d1bf95f3523ebff73a87.

--- SolrNet/Impl/ResponseParsers/ShardInfoResponseParser.cs
+++ SolrNet/Impl/ResponseParsers/ShardInfoResponseParser.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using SolrNet.Utils;
-using System.Globalization;
-using System.Xml.XPath;
 
 namespace SolrNet.Impl.ResponseParsers
 {
@@ -21,7 +19,7 @@ namespace SolrNet.Impl.ResponseParsers
         {
             var rootNode = xml.XPathSelectElement("response/lst[@name='shards.Info']");
             if (rootNode != null)
-                results.ShardInfoResults = ParseDocuments(rootNode).ToList();
+                results.TermVectorResults = ParseDocuments(rootNode).ToList();
         }
 
 
@@ -60,58 +58,7 @@ namespace SolrNet.Impl.ResponseParsers
                 .SelectMany(ParseField)
                 .ToList();
 
-            return new ShardInfoResult();
-        }
-
-        private IEnumerable<TermVectorResult> ParseField(XElement fieldNode)
-        {
-            return fieldNode.Elements()
-                .Select(termNode => ParseTerm(termNode, fieldNode.Attribute("name").Value));
-        }
-
-        private TermVectorResult ParseTerm(XElement termNode, string fieldName)
-        {
-            var nameValues = termNode.Elements()
-                .Select(e => new { name = e.Attribute("name").Value, value = e.Value })
-                .ToList();
-
-            var tf = nameValues
-                .Where(x => x.name == "tf")
-                .Select(x => (int?)int.Parse(x.value))
-                .FirstOrDefault();
-
-            var df = nameValues
-                .Where(x => x.name == "df")
-                .Select(x => (int?)int.Parse(x.value))
-                .FirstOrDefault();
-
-            var tfidf = nameValues
-                .Where(x => x.name == "tf-idf")
-                .Select(x => (double?)double.Parse(x.value, CultureInfo.InvariantCulture.NumberFormat))
-                .FirstOrDefault();
-
-            var offsets = termNode.Elements().SelectMany(ParseOffsets).ToList();
-            var positions = termNode.Elements().SelectMany(ParsePositions).ToList();
-
-            return new TermVectorResult(fieldName,
-                term: termNode.Attribute("name").Value,
-                tf: tf, df: df, tfIdf: tfidf,
-                offsets: offsets, positions: positions);
-        }
-
-        private IEnumerable<int> ParsePositions(XElement valueNode)
-        {
-            return from e in new[] { valueNode }
-                   where e.Attribute("name").Value == "positions"
-                   from p in e.Elements()
-                   select int.Parse(p.Value);
-        }
-
-        private IEnumerable<Offset> ParseOffsets(XElement valueNode)
-        {
-            return from e in valueNode.Elements()
-                   where e.Attribute("name").Value == "start"
-                   select new Offset(start: int.Parse(e.Value), end: int.Parse(((XElement)e.NextNode).Value));
+            return new TermVectorDocumentResult(uniqueKey, termVectorResults);
         }
     }
 }

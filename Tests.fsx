261f4455662c238cfdc4475d6b696c2b05daa65d
mausch <mauricioscheffer@gmail.com>
2013-05-13 02:01:55 -0300

    Run tests from fsi

---
+++ Tests.fsx
@@ -0,0 +1,11 @@
+﻿#I "lib"
+#r @"gallio.dll"
+#r @"MbUnit.dll"
+#r @"packages\Fuchu.0.2.2\lib\net40-client\Fuchu.dll"
+#r @"SolrNet.Tests\bin\Debug\SolrNet.Tests.dll"
+
+open Fuchu
+open SolrNet.Tests
+
+let test = Fuchu.MbUnit.MbUnitTestToFuchu typeof<DateTimeOffsetFieldParserTests>
+run test

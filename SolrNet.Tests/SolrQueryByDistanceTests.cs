592b94d2dbb1ae249f532abd0a5a95e269f3791f
mausch <mauricioscheffer@gmail.com>
2013-06-27 01:08:46 -0300

    Code reformat; avoid MbUnit-specific attributes

--- SolrNet.Tests/SolrQueryByDistanceTests.cs
+++ SolrNet.Tests/SolrQueryByDistanceTests.cs
@@ -1,43 +1,35 @@
+using System;
 using MbUnit.Framework;
-using SolrNet.Impl.FieldSerializers;
-using SolrNet.Impl.QuerySerializers;
 
-namespace SolrNet.Tests
-{
+namespace SolrNet.Tests {
     [TestFixture]
-    public class SolrQueryByDistanceTests
-    {
+    public class SolrQueryByDistanceTests {
         [Test]
-        [ExpectedArgumentNullException]
-        public void NullField_should_throw()
-        {
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void NullField_should_throw() {
             var q = new SolrQueryByDistance(null, 45.15, -93.85, 5);
         }
 
         [Test]
-        [ExpectedArgumentOutOfRangeException]
-        public void NegativeDistance_should_throw()
-        {
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeDistance_should_throw() {
             var q = new SolrQueryByDistance("store", 45.15, -93.85, -100);
         }
 
         [Test]
-        public void DefaultAccuracy_is_radius() 
-        {
+        public void DefaultAccuracy_is_radius() {
             var q = new SolrQueryByDistance("store", 45.15, -93.85, 5);
             Assert.AreEqual(CalculationAccuracy.Radius, q.Accuracy);
         }
 
         [Test]
-        public void Basic() 
-        {
+        public void Basic() {
             var q = new SolrQueryByDistance("store", 45.15, -93.85, 5.2);
-            Assert.AreEqual("{!geofilt pt=45.15,-93.85 sfield=store d=5.2}",q.Query);
+            Assert.AreEqual("{!geofilt pt=45.15,-93.85 sfield=store d=5.2}", q.Query);
         }
 
         [Test]
-        public void Basic_lower_accuracy()
-        {
+        public void Basic_lower_accuracy() {
             var q = new SolrQueryByDistance("store", 45.15, -93.85, 5, CalculationAccuracy.BoundingBox);
             Assert.AreEqual("{!bbox pt=45.15,-93.85 sfield=store d=5}", q.Query);
         }

dd8534037bc2ca561b20cf0c4303594aed234e91
mausch <mauricioscheffer@gmail.com>
2013-06-27 01:46:00 -0300

    fsi test runner

--- Tests.fsx
+++ Tests.fsx
@@ -4,8 +4,17 @@
 #r @"packages\Fuchu.0.2.2\lib\net40-client\Fuchu.dll"
 #r @"SolrNet.Tests\bin\Debug\SolrNet.Tests.dll"
 
+open System
 open Fuchu
 open SolrNet.Tests
 
-let test = Fuchu.MbUnit.MbUnitTestToFuchu typeof<SolrQueryResultsParserTests>
+
+let test = 
+    typeof<SolrQueryResultsParserTests>.Assembly.GetExportedTypes()
+    |> Seq.filter (fun (t: Type) -> not t.IsAbstract)
+    |> Seq.map Fuchu.MbUnit.MbUnitTestToFuchu
+    |> TestList
+
+
+//let test = Fuchu.MbUnit.MbUnitTestToFuchu typeof<SolrQueryResultsParserTests>
 run test

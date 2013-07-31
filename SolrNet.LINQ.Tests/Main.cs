107415707f07e26198e127997bc323fd129d18de
mausch <mauricioscheffer@gmail.com>
2013-07-31 00:40:35 -0300

    Fuchu

---
+++ SolrNet.LINQ.Tests/Main.cs
@@ -0,0 +1,10 @@
+﻿using Fuchu;
+
+namespace SolrNet.LINQ.Tests {
+    internal class Main1 {
+        public static void Main() {
+            var test = Fuchu.MbUnit.MbUnitTestToFuchu(typeof (QueryableSolrNetTester));
+            test.Run();
+        }
+    }
+}

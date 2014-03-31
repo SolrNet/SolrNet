80731b6a1a1cf2c9f358f1b9565ac66eeabd81ed
PaulSample <psam1508@gmail.com>
2014-03-31 11:24:13 -0400

    Update SolrFacetDateQuerySerializer.cs

--- SolrNet/Impl/FacetQuerySerializers/SolrFacetDateQuerySerializer.cs
+++ SolrNet/Impl/FacetQuerySerializers/SolrFacetDateQuerySerializer.cs
@@ -46,6 +46,9 @@ namespace SolrNet.Impl.FacetQuerySerializers {
             if (q.Other != null && q.Other.Count > 0)
                 foreach (var o in q.Other)
                     yield return KV.Create(string.Format("f.{0}.facet.date.other", fieldWithoutLocalParams), o.ToString());
+            if (q.Include != null && q.Include.Count > 0)
+                foreach (var i in q.Include)
+                    yield return KV.Create(string.Format("f.{0}.facet.date.include", fieldWithoutLocalParams), i.ToString());
         }
     }
 }

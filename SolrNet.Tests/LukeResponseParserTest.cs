2ea38715814f5ab4b22d184942c952476f403457
Christoph U <christoph.uhland@gmail.com>
2016-02-18 18:25:09 +0100

    Added some tests.

---
+++ SolrNet.Tests/LukeResponseParserTest.cs
@@ -0,0 +1,24 @@
+﻿// 
+using MbUnit.Framework;
+using SolrNet.Schema;
+using SolrNet.Tests.Utils;
+
+namespace SolrNet.Tests 
+{
+    [TestFixture]
+    public class LukeResponseParserTest 
+    {
+
+        [Test]
+        public void SolrFieldTypeParsing()
+        {
+            var schemaParser = new LukeResponseParser();
+            var xml = EmbeddedResource.GetEmbeddedXml(GetType(), "Resources.luke.xml");
+            LukeResponse lukeResponse = schemaParser.Parse(xml);
+
+            Assert.AreEqual(2, lukeResponse.SolrFields.Count);
+            Assert.AreEqual("id", lukeResponse.SolrFields[0].Name);
+            Assert.AreEqual("string", lukeResponse.SolrFields[0].Type.Name);
+        }
+    }
+}
--- SolrNet/Schema/LukeResponse.cs
+++ SolrNet/Schema/LukeResponse.cs
@@ -1,6 +1,19 @@
 ﻿// 
-namespace SolrNet.Schema {
-    public class LukeResponse {
-         
+
+using System.Collections.Generic;
+
+namespace SolrNet.Schema 
+{
+    /// <summary>
+    /// Represents a admin/luke response
+    /// </summary>
+    public class LukeResponse 
+    {
+
+        /// <summary>
+        /// Gets or sets the solr fields.
+        /// </summary>
+        /// <value>The solr fields.</value>
+        public List<SolrField> SolrFields { get; set; }
     }
 }

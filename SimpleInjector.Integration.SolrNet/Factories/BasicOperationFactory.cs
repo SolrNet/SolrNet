6028538f378dbf0d10865baeb80db4dde83f0327
HaMeD <shirzadpoor@hotmail.com>
2017-02-03 10:22:23 +0330

    simple injector integration implementation

---
+++ SimpleInjector.Integration.SolrNet/Factories/BasicOperationFactory.cs
@@ -0,0 +1,38 @@
+﻿using SolrNet;
+using SolrNet.Impl;
+using SolrNet.Schema;
+using System;
+
+namespace SimpleInjector.Integration.SolrNet.Factories
+{
+    class BasicOperationFactory
+    {
+        private readonly Container _Container;
+
+        public BasicOperationFactory(Container container)
+        {
+            _Container = container;
+        }
+
+        public object Create(SolrCore core)
+        {
+            var outputType = typeof(SolrBasicServer<>).MakeGenericType(core.DocumentType);
+            var queryExecuterType = typeof(ISolrQueryExecuter<>).MakeGenericType(core.DocumentType);
+            var docSerializerType = typeof(ISolrDocumentSerializer<>).MakeGenericType(core.DocumentType);
+
+            var output = Activator.CreateInstance(type: outputType,
+                args: new[] {
+                        new SolrConnection(core.ConnectionUrl),
+                        _Container.GetInstance(queryExecuterType),
+                        _Container.GetInstance(docSerializerType),
+                        _Container.GetInstance<ISolrSchemaParser>(),
+                        _Container.GetInstance<ISolrHeaderResponseParser>(),
+                        _Container.GetInstance<ISolrQuerySerializer>(),
+                        _Container.GetInstance<ISolrDIHStatusParser>(),
+                        _Container.GetInstance<ISolrExtractResponseParser>()
+                });
+
+            return output;
+        }
+    }
+}
---
+++ SimpleInjector.Integration.SolrNet/IConnectionBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleInjector.Integration.SolrNet
+{
+    /// <summary>
+    /// SolrNet connection URL builder interface.
+    /// </summary>
+    public interface IConnectionBuilder
+    {
+        /// <summary>
+        /// Builds connection url based on entity type.
+        /// </summary>
+        /// <typeparam name="TEntity">Type of entity</typeparam>
+        /// <returns>Connection string</returns>
+        string Build<TEntity>();
+
+        /// <summary>
+        /// Builds connection url based on entity type.
+        /// </summary>
+        /// <param name="entityType">Entity type</param>
+        /// <returns>Connection string</returns>
+        string Build(Type entityType);
+    }
+}

d9fb07564793896ad0911ba743b023395bde479c
Srihari Kothapalli <srihari@snovasys.com>
2017-07-16 01:59:03 +0100

    support for facet_ranges while parsing the results

--- SolrNet/Impl/FacetQuerySerializers/SolrFacetDateQuerySerializer.cs
+++ SolrNet/Impl/FacetQuerySerializers/SolrFacetDateQuerySerializer.cs
@@ -37,18 +37,18 @@ namespace SolrNet.Impl.FacetQuerySerializers {
 
         public override IEnumerable<KeyValuePair<string, string>> Serialize(SolrFacetDateQuery q) {
             var fieldWithoutLocalParams = localParamsRx.Replace(q.Field, ""); 
-            yield return KV.Create("facet.date", q.Field);
-            yield return KV.Create(string.Format("f.{0}.facet.date.start", fieldWithoutLocalParams), SerializeSingle(q.Start));
-            yield return KV.Create(string.Format("f.{0}.facet.date.end", fieldWithoutLocalParams), SerializeSingle(q.End));
-            yield return KV.Create(string.Format("f.{0}.facet.date.gap", fieldWithoutLocalParams), q.Gap);
+            yield return KV.Create("facet.range", q.Field);
+            yield return KV.Create(string.Format("f.{0}.facet.range.start", fieldWithoutLocalParams), SerializeSingle(q.Start));
+            yield return KV.Create(string.Format("f.{0}.facet.range.end", fieldWithoutLocalParams), SerializeSingle(q.End));
+            yield return KV.Create(string.Format("f.{0}.facet.range.gap", fieldWithoutLocalParams), q.Gap);
             if (q.HardEnd.HasValue)
-                yield return KV.Create(string.Format("f.{0}.facet.date.hardend", fieldWithoutLocalParams), SerializeSingle(q.HardEnd.Value));
+                yield return KV.Create(string.Format("f.{0}.facet.range.hardend", fieldWithoutLocalParams), SerializeSingle(q.HardEnd.Value));
             if (q.Other != null && q.Other.Count > 0)
                 foreach (var o in q.Other)
-                    yield return KV.Create(string.Format("f.{0}.facet.date.other", fieldWithoutLocalParams), o.ToString());
+                    yield return KV.Create(string.Format("f.{0}.facet.range.other", fieldWithoutLocalParams), o.ToString());
             if (q.Include != null && q.Include.Count > 0)
                 foreach (var i in q.Include)
-                    yield return KV.Create(string.Format("f.{0}.facet.date.include", fieldWithoutLocalParams), i.ToString());
+                    yield return KV.Create(string.Format("f.{0}.facet.range.include", fieldWithoutLocalParams), i.ToString());
         }
     }
 }

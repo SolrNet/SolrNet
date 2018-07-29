f0d833554ce21822639093ebbc37d16569122b69
João Martins <joao@dsd.me>
2018-07-29 22:05:27 +0100

    added SimpleInjector support
    - added module to support Simple Injector

---
+++ SimpleInjector.SolrNet.Tests/SimpleInjectorFixture.cs
@@ -0,0 +1,89 @@
+using SolrNet;
+using SolrNet.Impl;
+using System;
+using Xunit;
+
+namespace SimpleInjector.SolrNet.Tests
+{
+    public class SimpleInjectorFixture
+    {
+        private readonly Container Container;
+
+        public SimpleInjectorFixture()
+        {
+            Container = new Container();
+            Container.AddSolrNet("http://localhost:8983/solr");
+        }
+
+        [Fact]
+        public void ResolveIReadOnlyMappingManager()
+        {
+            var mapper = Container.GetInstance<IReadOnlyMappingManager>();
+            Assert.NotNull(mapper);
+        }
+
+
+        [Fact]
+        public void ResolveSolrDocumentActivator()
+        {
+            var solr = Container.GetInstance<ISolrDocumentActivator<Entity>>();
+            Assert.NotNull(solr);
+        }
+
+        [Fact]
+        public void ResolveSolrAbstractResponseParser()
+        {
+            var solr = Container.GetInstance<ISolrAbstractResponseParser<Entity>>();
+            Assert.NotNull(solr);
+        }
+
+
+        [Fact]
+        public void ResolveSolrAbstractResponseParsersViaEnumerable()
+        {
+            var result = Container.GetAllInstances<ISolrAbstractResponseParser<Entity>>();
+            Assert.NotNull(result);
+            Assert.Single(result);
+
+        }
+
+        [Fact]
+        public void ResolveSolrMoreLikeThisHandlerQueryResultsParser()
+        {
+            var solr = Container.GetInstance<ISolrMoreLikeThisHandlerQueryResultsParser<Entity>>();
+            Assert.NotNull(solr);
+        }
+
+
+        [Fact]
+        public void ResolveSolrOperations()
+        {
+            var solr = Container.GetInstance<ISolrOperations<Entity>>();
+            Assert.NotNull(solr);
+        }
+
+
+
+        [Fact]
+        public void SetBasicAuthenticationHeader()
+        {
+            var c = new Container();
+
+            //my credentials
+            var credentials = System.Text.Encoding.ASCII.GetBytes("myUsername:myPassword");
+            //in base64
+            var credentialsBase64 = Convert.ToBase64String(credentials);
+            //use the options to set the Authorization header.
+            c.AddSolrNet("http://localhost:8983/solr", options => { options.HttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentialsBase64); });
+
+            //test
+            var connection = c.GetInstance<ISolrConnection>() as AutoSolrConnection;
+
+            Assert.NotNull(connection.HttpClient.DefaultRequestHeaders.Authorization);
+            Assert.Equal(credentialsBase64, connection.HttpClient.DefaultRequestHeaders.Authorization.Parameter);
+        }
+
+        public class Entity { }
+
+    }
+}
---
+++ SimpleInjector.SolrNet/SolrCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleInjector.SolrNet
+{
+    internal class SolrCore
+    {
+        public string Id { get; private set; }
+        public string Url { get; private set; }
+        public Type DocumentType { get; private set; }
+
+        public SolrCore(string id, Type documentType, string url)
+        {
+            Id = id;
+            DocumentType = documentType;
+            Url = url;
+        }
+    }
+}

47889d240f362289544ee85d54e7f901882795ef
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-02-14 16:18:54 +0000

    #544 Use the response body from the failed post for the exception message in AutoSolrConnection

--- SolrNet/Impl/AutoSolrConnection.cs
+++ SolrNet/Impl/AutoSolrConnection.cs
@@ -91,7 +91,7 @@ namespace SolrNet.Impl
                 response = await HttpClient.GetAsync(u.Uri, cancellationToken);
 
             if (!response.IsSuccessStatusCode)
-                throw new SolrConnectionException($"{response.StatusCode}: {response.ReasonPhrase}", null, u.Uri.ToString());
+                throw new SolrConnectionException(await response.Content.ReadAsStringAsync(), null, u.Uri.ToString());
 
             return await response.Content.ReadAsStreamAsync();
 
@@ -144,7 +144,7 @@ namespace SolrNet.Impl
             var response = await HttpClient.PostAsync(u.Uri, sc);
 
             if (!response.IsSuccessStatusCode)
-                throw new SolrConnectionException($"{response.StatusCode}: {response.ReasonPhrase}", null, u.Uri.ToString());
+                throw new SolrConnectionException(await response.Content.ReadAsStringAsync(), null, u.Uri.ToString());
 
             return await response.Content.ReadAsStreamAsync();
         }

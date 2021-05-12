843a1893d47c0195d8a319f52c0137a8b9d1a220
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-05-12 23:29:29 +0100

    Downgrade docker-compose to 1.28.2
    as the version that comes with the Ubuntu environment hangs
    https://github.com/actions/virtual-environments/issues/3367

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-docker-compose -f integration-tests-compose.yml up --build --exit-code-from integration_tests --abort-on-container-exit --force-recreate
+alias docker-compose="nix run -f https://github.com/NixOS/nixpkgs/archive/bed08131cd29a85f19716d9351940bdc34834492.tar.gz docker-compose -c docker-compose "
+
+docker-compose --version
+
+docker-compose -f integration-tests-compose.yml up --build --exit-code-from integration_tests --abort-on-container-exit --force-recreate --no-color --renew-anon-volumes
 

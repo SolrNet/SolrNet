db3e91304e0ef4e32fcb726cfe52fc9e1c4bd446
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-09-19 11:00:51 +0100

    Don't use set-env
    https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/

--- pack.sh
+++ pack.sh
@@ -12,7 +12,9 @@ if [ -n "$GITHUB_TOKEN" ]; then
     dotnet nuget push --source "github" $nupkg &
   done
   wait
-  echo "::set-env name=VersionTag::$Version" # for github actions
+  if [ -n "$GITHUB_ENV" ]; then
+    echo "VersionTag=$Version" >> $GITHUB_ENV  # for github actions
+  fi
 else
   echo "GITHUB_TOKEN not defined, won't push packages"
 fi

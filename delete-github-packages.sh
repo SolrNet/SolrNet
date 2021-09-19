457e86bb94aa6aa71806e55e0b4ecc8d0f2aaa94
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-09-19 17:12:32 +0100

    Script to delete all nuget packages from github

---
+++ delete-github-packages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S nix run -c sh
+set -eu
+
+function github {
+  local path=$1
+  shift
+  curl -s -u mausch:$GITHUB_TOKEN -H 'Accept: application/vnd.github.v3+json' https://api.github.com${path} $@
+}
+
+packages="$(github /orgs/SolrNet/packages?package_type=nuget | jq -r '.[].name')"
+
+for p in $packages; do
+  echo Deleting $p
+  github /orgs/SolrNet/packages/nuget/$p -XDELETE
+done

c0b1f3c7f64d85eb702b0d143c9a2a327923554b
Jevgeni Geurtsen <jevgeni.geurtsen@weareyou.com>
2022-03-01 13:04:06 +0100

    Add LightInject support

---
+++ LightInject.SolrNet.Tests/LightInjectIntegrationFixture.cs
@@ -0,0 +1,43 @@
+﻿// 
+
+using System;
+using Xunit;
+using SolrNet;
+using Xunit.Abstractions;
+
+namespace LightInject.SolrNet.Tests
+{
+    
+    [Trait("Category","Integration")]
+    public class LightInjectIntegrationFixture 
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        private readonly IServiceContainer defaultServiceProvider;
+
+        public LightInjectIntegrationFixture(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+            
+            this.defaultServiceProvider = new ServiceContainer();
+            this.defaultServiceProvider.AddSolrNet("http://localhost:8983/solr/techproducts");
+        }
+
+
+        [Fact]
+        public void Ping_And_Query()
+        {
+            var solr = defaultServiceProvider.GetInstance<ISolrOperations<LightInjectFixture.Entity>>();
+            solr.Ping();
+            testOutputHelper.WriteLine(solr.Query(SolrQuery.All).Count.ToString());
+        }
+
+        [Fact]
+        public void Ping_And_Query_SingleCore()
+        {
+            var solr = defaultServiceProvider.GetInstance<ISolrOperations<LightInjectFixture.Entity>>();
+            solr.Ping();
+            testOutputHelper.WriteLine(solr.Query(SolrQuery.All).Count.ToString());
+        }
+
+    }
+}

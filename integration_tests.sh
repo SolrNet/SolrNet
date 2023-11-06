65494d07c64421ce759ec2de9640d0b8ae50cf39
Mauricio Scheffer <mauricioscheffer@gmail.com>
2023-11-06 20:38:57 +0000

    Add missing --accept-flake-config
    I think devenv needs it

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env -S nix --extra-experimental-features "nix-command flakes" shell .# -c sh
+#!/usr/bin/env -S nix --extra-experimental-features "nix-command flakes" --accept-flake-config shell .# -c sh
 
 dotnet test --filter 'Category=Integration&FullyQualifiedName!~Cloud' --logger html

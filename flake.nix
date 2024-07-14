e942fb772e3b4b05b6f0b54d9a840948df956436
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-07-14 17:46:19 +0100

    Update flakes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };

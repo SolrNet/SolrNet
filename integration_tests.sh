d477aa8cad27bd295815fb0a4c61ee2c1f69c665
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-07-16 22:16:26 +0100

    Use nicer test reports

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S nix --extra-experimental-features "nix-command flakes" --accept-flake-config shell .# -c sh
 
-dotnet test --filter 'Category=Integration&FullyQualifiedName!~Cloud' --logger html
+dotnet test --filter 'Category=Integration&FullyQualifiedName!~Cloud' --logger "trx;LogFileName=test-results.trx"
--- unit_tests.sh
+++ unit_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S nix --extra-experimental-features "nix-command flakes" --accept-flake-config shell .# -c sh
 
-dotnet test --filter 'Category!~Integration' --logger html
+dotnet test --filter 'Category!~Integration' --logger "trx;LogFileName=test-results.trx"

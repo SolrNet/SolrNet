ed269467f7193428f29396c5173f970f96a0f44d
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2025-02-02 20:28:26 +0100

    add basic otel test

--- Microsoft.DependencyInjection.SolrNet.Tests/MicrosoftDependencyIntegrationFixture.cs
+++ Microsoft.DependencyInjection.SolrNet.Tests/MicrosoftDependencyIntegrationFixture.cs
@@ -1,10 +1,14 @@
 ﻿// 
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Xunit;
 using SolrNet;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using SolrNet.Impl;
 using SolrNet.Tests.Common;
 using Xunit.Abstractions;
 
@@ -43,5 +47,40 @@ namespace Microsoft.DependencyInjection.SolrNet.Tests
             testOutputHelper.WriteLine(solr.Query(SolrQuery.All).Count.ToString());
         }
 
+        [Fact]
+        public void BasicOpenTelemetryTest()
+        {
+            // Arrange
+            Activity oTelActivity = null;
+            var listener = new ActivityListener
+            {
+                ActivityStarted = _ => { },
+                ActivityStopped = activity => oTelActivity = activity,
+                ShouldListenTo = activitySource => activitySource.Name == DiagnosticHeaders.DefaultSourceName,
+                Sample = (ref ActivityCreationOptions<ActivityContext> _) => ActivitySamplingResult.AllData
+            };
+            ActivitySource.AddActivityListener(listener);
+
+            var solr = DefaultServiceProvider.GetService<ISolrOperations<MicrosoftDependencyFixture.Entity>>();
+            
+            // Act
+            solr.Query(SolrQuery.All);
+            
+            // Assert
+            Assert.NotNull(oTelActivity);
+            Assert.Equal(ActivityKind.Client, oTelActivity.Kind);
+            Assert.Equal("query", oTelActivity.OperationName);
+            Assert.Equal("query", oTelActivity.DisplayName);
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "db.system", Value: "solr" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "db.query.text", Value: """[{"q":"*:*"},{"rows":"100000000"}]""" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "db.collection.name", Value: "techproducts" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "server.address", Value: "127.0.0.1" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "server.port" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "solr.status" });
+            Assert.Contains(oTelActivity.TagObjects, _ => _ is { Key: "solr.qtime" });
+            Assert.Contains(oTelActivity.TagObjects,
+                _ => _.Key == "url.full" && Regex.IsMatch(_.Value as string ?? "",
+                    @"http:\/\/127\.0\.0\.1:\d+\/solr\/techproducts\/select"));
+        }
     }
 }
